1d033ed19eaa8c355a7f34e6975339b3861c00f0
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-25 18:49:37 +0600

    Add image and transformation as I need!

--- app/src/main/java/com/jubayer_ahamad_tayef/coil/MainActivity.kt
+++ app/src/main/java/com/jubayer_ahamad_tayef/coil/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.jubayer_ahamad_tayef.coil.ui.theme.CoilTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +27,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CoilImage()
                 }
             }
         }
@@ -31,17 +40,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CoilImage() {
+    Box(modifier = Modifier.size(150.dp), contentAlignment = Alignment.Center) {
+        val painter =
+            rememberImagePainter(data = "https://avatars.githubusercontent.com/u/129745706?v=4",
+                builder = {
+                    placeholder(R.drawable.image_placeholder)
+                    error(R.drawable.ic_error)
+                    crossfade(1000)
+                    transformations(
+//                    GrayscaleTransformation(),
+                        CircleCropTransformation(),
+//                    BlurTransformation(LocalContext.current)
+                    )
+                })
+        val painterState = painter.state
+        Image(painter = painter, contentDescription = "My Image")
+        if (painterState is ImagePainter.State.Loading) CircularProgressIndicator()
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    CoilTheme {
-        Greeting("Android")
-    }
+fun CoilImagePreview() {
+
 }
